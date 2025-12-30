20ad24b7f94b7d0e62e11cdfc9b9d3be7ff32029
Kingshuk Paul <kingshukpaul133569d@gmail.com>
2025-12-30 20:59:11 +0000

    Creating SwiftBot Instance

--- CS1704_SwiftBotWork/src/main/java/SearchForLight.java
+++ CS1704_SwiftBotWork/src/main/java/SearchForLight.java
@@ -1,8 +1,14 @@
+import swiftbot.SwiftBotAPI;
 
 public class SearchForLight {
-
+	static SwiftBotAPI swiftBot;
 	public static void main(String[] args) {
-
+		try {
+			swiftBot = SwiftBotAPI.INSTANCE;
+		} catch (Exception e) {
+			System.out.println("\nI2C disabled!");
+			System.exit(5);
+		}
 	}
 
 }
