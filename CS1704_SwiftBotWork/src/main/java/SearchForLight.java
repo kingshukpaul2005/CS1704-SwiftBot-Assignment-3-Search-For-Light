d49c4fc717c6cda1e30ea09d7a7efbd6ccb29e57
Kingshuk Paul <kingshukpaul133569d@gmail.com>
2025-12-30 21:16:52 +0000

    Standby Loop created with Button assignment (Errors)

--- CS1704_SwiftBotWork/src/main/java/SearchForLight.java
+++ CS1704_SwiftBotWork/src/main/java/SearchForLight.java
@@ -1,14 +1,24 @@
+import swiftbot.Button;
 import swiftbot.SwiftBotAPI;
 
 public class SearchForLight {
 	static SwiftBotAPI swiftBot;
 	public static void main(String[] args) {
+		//Initialize the SwiftBotAPI with exception
 		try {
 			swiftBot = SwiftBotAPI.INSTANCE;
 		} catch (Exception e) {
 			System.out.println("\nI2C disabled!");
 			System.exit(5);
 		}
-	}
 
+		//StandBy Loop: Until the button isnt pressed
+		boolean standBy = true;
+		swiftBot.enableButton(Button.A, () -> {standBy = false;});
+		while (standBy) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {}
+		}
+	}
 }
